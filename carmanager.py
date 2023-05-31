9317ebdd4dd82a3ed4cf8b6af7c90bdc4cb4a3d9
Dilip Gummadi <dilip.calgry@gmail.com>
2023-05-31 13:24:37 +0530

    created carmanager() added create_car() and move_car() logic

---
+++ carmanager.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+from random import choice, randint
+import time
+colors = ['red', 'green', 'cyan', 'blue', 'grey', 'brown', 'yellow']
+class carManager():
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        if randint(1,6) == 1:
+            new_car = Turtle()
+            new_car.color(choice(colors))
+            new_car.shape('square')
+            new_car.penup()
+            new_car.shapesize(1,2,0)
+            new_car.goto(300,randint(-250,250))
+            self.cars.append(new_car)
+
+    def move_car(self):
+        for car in self.cars:
+            car.backward(10)
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from player import Player
+from carmanager import carManager
+import time
 
 screen = Screen()
 screen.title("Turtle running Game 🐢🐢")
@@ -8,10 +10,13 @@ screen.tracer(0)
 new_player = Player()
 screen.listen()
 screen.onkey(new_player.move,"Up")
-
+cars = carManager()
 
 game_on =True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    cars.create_car()
+    cars.move_car()
 
 screen.exitonclick()
